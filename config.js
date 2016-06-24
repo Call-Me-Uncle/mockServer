3c06b1d8c6f4883c3f1070dd5f14934caa7b9f69
mioji <jiaoyuxin@mioji.com>
2016-06-24 15:36:45 +0800

    sql persion mk

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mysqlConfig: {
     connectionLimit : 10,
-    host            : '123.59.80.107',
-    user            : 'root',
+    host            : '10.10.222.19',
+    user            : 'writer',
     password        : 'miaoji1109',
     database        : 'mioji_chat_log'
   }
